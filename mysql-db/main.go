db81a5d5ff3ebb355abe5e4dc35955d928887fd8
Kendrick Ang <kendrick.wh@outlook.com>
2020-12-09 11:25:56 +0800

    implement simple read from db

--- mysql-db/main.go
+++ mysql-db/main.go
@@ -2,18 +2,41 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"example.com/kendrick/protocol"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
 
-func main() {
-	fmt.Println("Go MySQL")
-	db, err := sql.Open("mysql", "myroot:mypassword@tcp(127.0.0.1:3306)/mydb")
+var db *sql.DB
 
+func getUsers() *sql.Rows {
+	res, err := db.Query("SELECT * FROM users")
 	if err != nil {
-		panic(err.Error())
+		log.Panicln(err)
 	}
-	defer db.Close()
+	return res
+}
+
+func connect() *sql.DB {
+	database, err := sql.Open("mysql", "root:MonsterHunter!@@tcp(localhost:3306)/users_db")
+	if err != nil {
+		log.Panicln(err.Error())
+	}
 	log.Println("Connected to MySQL database")
+	return database
+}
+
+func main() {
+	db = connect()
+	defer db.Close()
+
+	rows := getUsers()
+	for rows.Next() {
+		var user protocol.User
+		err := rows.Scan(&user.Nickname, &user.Username, &user.PwHash, &user.ProfilePic)
+		if err != nil {
+			log.Panicln(err)
+		}
+		log.Println(user)
+	}
 }
